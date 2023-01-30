1428fc0c3bf641d2f6e00865c04de98ebad52951
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-30 15:36:35 +0000

    [start]favorites

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
@@ -10,6 +11,10 @@ class Book < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 
 
