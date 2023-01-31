21edec17606649762fac777afeaf4a88e517aeb4
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-31 15:42:50 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book_comment = BookComment.new
     @books = @user.books
     @book = Book.new
   end
