97a957c1733b57153ba359b20e8fc3ae9cee88d3
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-05 07:49:05 +0000

    Revert "[finish]"
    This reverts commit b597b480bc3db400c0b3b62de3845a72ab262d74.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,6 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
-    @word = params[:word]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
