93970a5297764dbb3a8f25bad49d6e146ebf805e
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-07 09:33:47 +0000

    [finish2]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,20 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
-    @comment.save
-    @book_comment = BookComment.new
-    render :book_comments  #render先にjsファイルを指定
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+    render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+    end
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    @book_comment.destroy
-    @book_comment = BookComment.new #投稿が残らないようにするため
-    render :book_comments  #render先にjsファイルを指定
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
