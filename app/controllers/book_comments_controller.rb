3e3de8bb1f5f49135df8d8d34a4c08c946b56f58
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-07 07:41:02 +0000

    [finish]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,26 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    @book_comment = BookComment.new
+    render :book_comments  #render先にjsファイルを指定
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new #投稿が残らないようにするため
+    render :book_comments  #render先にjsファイルを指定
   end
 
   private
 
-   def book_comment_params
-     params.require(:book_comment).permit(:comment)
-   end
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
 
 end
-
-
-
-
