2efb063aa9d0ea4296ce195fc4ca2db68fc73649
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-09 15:03:03 +0000

    [finish2]応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,14 @@ class BooksController < ApplicationController
     @book_comment = BookComment.new
     @booknew = Book.new
     @user = @book.user
-    @book_detail = Book.find(params[:id])
-    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-      current_user.view_counts.create(book_id: @book_detail.id)
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
     end
   end
 
   def index
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
+    to = Time.current.at_beginning_of_day
+    from = (to - 6.day).at_end_of_day
     #上の２行で一週間分のデータ、を表す
     @books = Book.includes(:favorites).sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
     #↑reverseをつけないと、少ない順に表示される
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,7 @@ class Favorite < ApplicationRecord
 
  belongs_to :user
  belongs_to :book
+ #↓過去１週間のいいね合計数に表示のサンプルコード
+ validates_uniqueness_of :book_id, scope: :user_id
 
 end
